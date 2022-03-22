537100d1e0e5d638943267aed680821501a630e5
PamLemus <lemusvillagranap@student.douglascollege.ca>
2022-03-22 14:26:46 -0700

    BMI Calculation added - Pamela Lemus

--- Application/Body&Beyond/app/src/main/java/com/example/bodybeyond/activities/HomeActivity.java
+++ Application/Body&Beyond/app/src/main/java/com/example/bodybeyond/activities/HomeActivity.java
@@ -15,6 +15,7 @@ import android.text.style.ForegroundColorSpan;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.example.bodybeyond.R;
 import com.example.bodybeyond.adapters.ViewPagerDietExerciseAdapter;
@@ -22,10 +23,22 @@ import com.example.bodybeyond.fragments.DietFragment;
 import com.example.bodybeyond.fragments.ExerciseFragment;
 import com.google.android.material.tabs.TabLayout;
 
+import java.text.DecimalFormat;
+
 public class HomeActivity extends AppCompatActivity {
 
     TabLayout tabLayoutDietExercise;
     ViewPager viewPagerDietExercise;
+
+    TextView BMIResult;
+
+    private static final DecimalFormat df = new DecimalFormat("0.0");
+
+
+    String gender;
+    double height = 187;
+    double weight = 95;
+    double BMIcalculation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +57,37 @@ public class HomeActivity extends AppCompatActivity {
         int black = Color.BLACK;
         setActionbarTextColor(actBar, black);
 
+        //Button Calculate Again Click Listener
+        Button calculate = findViewById(R.id.btnCalculateAgain);
+        calculate.setOnClickListener((View view) -> {
+            startActivity(new Intent(HomeActivity.this, CalculateBMIActivity.class));
+
+        });
+
+       //Porgress Bar configuration
+        ProgressBar progressBarSteps = findViewById(R.id.progressBarSteps);
+        progressBarSteps.setMax(100);  // target steps
+        progressBarSteps.setProgress(80);  //steps walked
+
+        //Configuration for TabLayout
+        tabLayoutDietExercise = findViewById(R.id.tabLayout);
+        viewPagerDietExercise = findViewById(R.id.viewPager);
+
+        tabLayoutDietExercise.setupWithViewPager(viewPagerDietExercise);
+        ViewPagerDietExerciseAdapter vpDEAdapter = new ViewPagerDietExerciseAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        vpDEAdapter.addFragmentsDE(new DietFragment(), "Diet");
+        vpDEAdapter.addFragmentsDE(new ExerciseFragment(), "Exercise");
+        viewPagerDietExercise.setAdapter(vpDEAdapter);
+
+
+        //Display BMI result
+
+        BMIResult = findViewById(R.id.txtBMIResult);
+        double BMIvalue = calculateBMI(height,weight);
+        BMIResult.setText(df.format(BMIvalue));
+
+
+
     }
 
     //Method to change text color from Action Bar
@@ -53,26 +97,14 @@ public class HomeActivity extends AppCompatActivity {
         Spannable spannablerTitle = new SpannableString(title);
         spannablerTitle.setSpan(new ForegroundColorSpan(color), 0, spannablerTitle.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         actBar.setTitle(spannablerTitle);
-
-        Button calculate = findViewById(R.id.btnCalculateAgain);
-        calculate.setOnClickListener((View view) -> {
-              startActivity(new Intent(HomeActivity.this, CalculateBMIActivity.class));
-
-        });
-
-        ProgressBar progressBarSteps = findViewById(R.id.progressBarSteps);
-        progressBarSteps.setMax(100);  // target steps
-        progressBarSteps.setProgress(80);  //steps walked
-
-        tabLayoutDietExercise = findViewById(R.id.tabLayoutDietExercise);
-        viewPagerDietExercise = findViewById(R.id.viePagerDietExercise);
-
-        tabLayoutDietExercise.setupWithViewPager(viewPagerDietExercise);
-        ViewPagerDietExerciseAdapter vpDEAdapter = new ViewPagerDietExerciseAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
-        vpDEAdapter.addFragmentsDE(new DietFragment(), "Diet");
-        vpDEAdapter.addFragmentsDE(new ExerciseFragment(), "Exercise");
-        viewPagerDietExercise.setAdapter(vpDEAdapter);
-
-
     }
+
+    public double calculateBMI (double height, double weight) {
+        double cmHeight = (height/100);
+        double powerHeight = Math.pow(cmHeight,2);
+        BMIcalculation = (weight/powerHeight);
+        return BMIcalculation;
+    }
+
+
 }
